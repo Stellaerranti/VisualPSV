a4af6fbca3df6de690574db47882bffa7fd80c7f
Stellaerranti <ushakovda@my.msu.ru>
2024-07-30 16:43:27 +0300

    base functions added

--- python/visualPSV.py
+++ python/visualPSV.py
@@ -1,7 +1,32 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jul 30 11:15:05 2024
+import tkinter as tk
+from tkinter import Label, Entry, Tk
+from tkinter import messagebox
 
-@author: Rudko
-"""
+window = Tk()
+window.title("Visual PSV")
+window.geometry('1200x900')
 
+lon_lb = Label(
+   window,
+   text="Longitude ",
+)
+lon_lb.grid(row=4, column=1)
+
+lon_tf = Entry(
+    window
+)
+lon_tf.grid(row = 5, column = 1)
+
+lat_lb = Label(
+   window,
+   text="Latitude ",
+)
+lat_lb.grid(row=6, column=1)
+
+lat_tf = Entry(
+    window
+)
+lat_tf.grid(row = 7, column = 1)
+
+#this piece of shit must be in the end
+window.mainloop()
